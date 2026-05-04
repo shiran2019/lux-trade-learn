79d077390b50dae13fafcc3235a36b3e5fc3163e
Pathiraja S.P.M.S.M.S. <88098649+shiran2019@users.noreply.github.com>
2026-05-04 10:16:34 +0530

    Update entry-client.tsx

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Router } from '@tanstack/react-router'
+import { RouterProvider } from '@tanstack/react-router'
 import { getRouter } from './router'
 
 const router = getRouter()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
